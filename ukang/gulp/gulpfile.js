d8133155395fa482ef52bf3054c70da80575008b
Lanny Lee <lanny.lee@foxmail.com>
2017-12-04 18:52:46 +0800

    Remove V0.2 files

--- ukang/gulp/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp'),
-    connect = require('gulp-connect');
-
-gulp.task('serve', function() {
-    connect.server({
-        root: '../www',
-        port: 9000
-    });
-});
